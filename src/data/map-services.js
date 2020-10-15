9a0757655dd2c53db4b6c0a78fabd181a05728de
jeffstahlnecker <jeff@stahlnecker.me>
2020-10-15 13:57:08 +0200

    fixed variable for netlify

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -13,6 +13,6 @@ export const mapServices = [
   {
     name: 'Mapbox',
     attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/namgyeongcho/ckg8vwf6202uk19p9qac4oevt/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`
+    url: `https://api.mapbox.com/styles/v1/namgyeongcho/ckg8vwf6202uk19p9qac4oevt/tiles/256/{z}/{x}/{y}@2x?access_token=${JSON.parse(`${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`)}`
   }
 ];
