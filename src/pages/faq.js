f1e011d83f84acc77e1d7b3776f3746a1d3f5ef3
stahldom <jeff@stahlnecker.me>
2020-10-23 17:56:03 +0200

    add tailwindcss, set up strapi, create faq page and component

---
+++ src/pages/faq.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Trans, useTranslation } from "gatsby-plugin-react-i18next";
+import FAQ from "../components/Faq";
+
+import Layout from "../components/Layout";
+import Container from "../components/Container";
+
+const FaqPage = () => {
+  const { t } = useTranslation();
+  return (
+    <Layout pageName="about">
+      <Helmet>
+        <title>{t("MXC Mapper FAQ")}</title>
+      </Helmet>
+      <Container type="content">
+        <FAQ />
+      </Container>
+    </Layout>
+  );
+};
+
+export default FaqPage;
