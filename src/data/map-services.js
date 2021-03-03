18ebadd42d0653ccadd64c3c11bd0387e45c67f0
NamGyeong <namgyeong.cho@mxc.org>
2021-03-03 10:43:05 +0100

    MMD-803 blocked mapbox temporary for prevent infinit loop

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,11 +10,11 @@ export const mapServices = [
     attribution: '&copy; NASA Blue Marble, image service by OpenGeo',
     url:
       'https://gibs-{s}.earthdata.nasa.gov/wmts/epsg3857/best/BlueMarble_ShadedRelief_Bathymetry/default//EPSG3857_500m/{z}/{y}/{x}.jpeg',
-  },
-  {
+  }/*,
+   {
     name: 'Mapbox',
     attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
     url: `https://api.mapbox.com/styles/v1/namgyeongcho/ckg8vwf6202uk19p9qac4oevt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmFtZ3llb25nY2hvIiwiYSI6ImNrZzduYzhjZDA4dTMyeG96aXR5OHdmZDQifQ.xmG3uiCV2noOHtgNUvCCTQ`
-  }
+  } */
 ];
 /* eslint-enable */
